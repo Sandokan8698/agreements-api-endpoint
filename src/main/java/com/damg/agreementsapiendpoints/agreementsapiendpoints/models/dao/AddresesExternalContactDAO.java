6fbaa8302367b0ff2edac3bbc97eae35c143b31e
Abdel Mendez <denysabdel@gmail.com>
2018-10-01 18:11:26 -0500

    Multiples Implementation

--- src/main/java/com/damg/agreementsapiendpoints/agreementsapiendpoints/models/dao/AddresesExternalContactDAO.java
+++ src/main/java/com/damg/agreementsapiendpoints/agreementsapiendpoints/models/dao/AddresesExternalContactDAO.java
@@ -3,5 +3,6 @@ package com.damg.agreementsapiendpoints.agreementsapiendpoints.models.dao;
 import com.damg.agreementsapiendpoints.agreementsapiendpoints.models.entities.AddresesExternalContacts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AddresesExternalContactDAO extends JpaRepository<AddresesExternalContacts, Long> {
 }
